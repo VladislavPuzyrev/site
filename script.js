a98c09b6d5fd3311ff2ebd31f049cfad12da41a7
VladislavPuzyrev <114629184+VladislavPuzyrev@users.noreply.github.com>
2022-10-18 00:54:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(".hide").hide();
+  
+  
+  $(".marker-russia").click(function(){
+    $(".hide").hide();
+    $(".russia-txt").fadeIn(300);
+  });
+  
+    $(".marker-usa").click(function(){
+       $(".hide").hide();
+    $(".usa-txt").fadeIn(300);
+  });
+  
+    $(".marker-uk").click(function(){
+       $(".hide").hide();
+    $(".uk-txt").fadeIn(300);
+  });
+  
+    $(".marker-brazil").click(function(){
+       $(".hide").hide();
+    $(".brazil-txt").fadeIn(300);
+  });
+
+});
